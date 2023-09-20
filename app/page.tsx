21beb39e89cc5a54814d134518819d368080d1fa
davidrotari19 <davidrotari9@gmail.com>
2023-09-20 23:40:35 +0300

    animation

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center pb-24">      
       <div className=' items-center justify-center mt-8 mb-6 text-center backdrop-blur-lg p-4 rounded-2xl ring-2 ring-gray-400 ring-opacity-10 w-[90dvw] flex flex-col bg-[#18181b]/80'>
         <p className='text-gray-300 text-sm leading-7'>
-          Studiu cercetare
+          Studiu de cercetare
         </p>
         <h2 className='text-4xl font-bold text-white  '>
           Titlu
